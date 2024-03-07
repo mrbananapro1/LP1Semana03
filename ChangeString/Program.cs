f7d1f2023eee5d1423b5e042a7b92ee4913800b9
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-07 20:37:47 +0000

    Requesting a phrase and a letter

---
+++ ChangeString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva algo");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Escreva uma letra");
+            string letra = Console.ReadLine();
+        }
+    }
+}
