f8bded328ab299fdb0376f51ccc47d29d549df37
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-07 20:25:54 +0000

    Foreach loop

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach ( string word in args)
+            {
+                if (word.Length > 6)
+                {
+                    break;
+                }
+
+                if (word.Length > 3)
+                {
+                    Console.WriteLine(word);
+                }
+
+            }
+        }
+    }
+}
