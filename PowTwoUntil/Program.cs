80ec956f780c30965627f7ce9bae067b0913bbb5
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-08 16:28:25 +0000

    For loop PowTwoUntil

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void PowersOf2Until5()
+        {
+            for(int i = 1; i <= (1 <<5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
